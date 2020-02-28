2abd2948eb363751001fdf40c902a15d912c2a8e
ovie umukoro <ovie.u.umukoro@gmail.com>
2020-02-27 21:36:05 -0500

    Test if minheapq class would compile

--- src/test/java/com/neo/proj2ab/ArrayHeapMinPQTest.java
+++ src/test/java/com/neo/proj2ab/ArrayHeapMinPQTest.java
@@ -4,4 +4,9 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class ArrayHeapMinPQTest {
+
+    @Test
+    public void buildTest() {
+        ArrayMinHeapPQ<Integer> heapq = new ArrayMinHeapPQ<>();
+    }
 }
