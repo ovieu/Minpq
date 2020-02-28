69a9f06ce890abe43b110fd733cf210350ebf9f0
ovie umukoro <ovie.u.umukoro@gmail.com>
2020-02-27 21:08:10 -0500

    Create the node class to hold priority elements

--- src/main/java/com/neo/proj2ab/ArrayMinHeapPQ.java
+++ src/main/java/com/neo/proj2ab/ArrayMinHeapPQ.java
@@ -30,4 +30,38 @@ public class ArrayMinHeapPQ<T> implements ExtrinsicMinPQ<T> {
     public void changePriority(T item, double priority) {
 
     }
+
+    private class Node {
+        T value;
+        double priority;
+
+        public Node(T value, double priority) {
+            this.value = value;
+            this.priority = priority;
+        }
+
+        public T getValue() {
+            return value;
+        }
+
+        public void setValue(T value) {
+            this.value = value;
+        }
+
+        public double getPriority() {
+            return priority;
+        }
+
+        public void setPriority(double priority) {
+            this.priority = priority;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "value=" + value +
+                    ", priority=" + priority +
+                    '}';
+        }
+    }
 }
